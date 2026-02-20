ef990356f1788451b75244ddcfe571bbe7869244
linaberhane <lina.berhane@stu.daviess.kyschools.us>
2026-02-20 04:28:59 +0000

    Commit

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -9,29 +9,97 @@
 
 public class CaesarCipher {
 
-	private char[][] alphabet;
-	private char[][] shifted;
+	private char[] alphabet;
+	private char[] shifted;
 	private int shift;
 
-    public CaesarCipher() {
+    public CaesarCipher() 
+    {
+        //populate the alphabet with a loop either using the ASCII table or make a string with the alphabet and take 
+        // chars from it like tictactoe.
+        alphabet = new char[26];
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        for(int i=0; i<26; i++)
+        {
+            alphabet[i] = alpha.charAt(i);
+        }
 
     }
 
-    public CaesarCipher(int num){
+    public CaesarCipher(int num)
+    {
+        //Fill out the alphabet array
+        alphabet = new char[26];
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        for(int i=0; i<26; i++)
+        {
+            alphabet[i] = alpha.charAt(i);
+        }
+
+        //Fill out the shifted array
+        shift = num;
+        shifter(num);
 
     }
 
-    public String encrypt(String message){
+    public String encrypt(String message) 
+    {
+        //look at that position in alphabet, then the same one in shifted. If it's there (letter), concatenate.
+        // Else, concatenate but don't encrypt
+        //Use modulus to shift
+        String output = "";
 
+        for(int i = 0; i < message.length(); i++)
+        {
+            char current = message.charAt(i);
+            int index = -1;
+
+            for(int alpha = 0; alpha < 26; alpha++)
+            {
+                if(current == alphabet[alpha])
+                   index = alpha;
+            }
+        
+        if(index == -1)
+            output += current;
+        else
+            output += shifted[index];
+    }
+        return output;
     }
 
-    public String decrypt(String message){
+    public String decrypt(String message)
+    {
+        //Same thing, but look at shifted first and then the same position in alphabet
+        String output = "";
 
+        for(int i = 0; i < message.length(); i++)
+        {
+            char current = message.charAt(i);
+            int index = -1;
+
+            for(int alpha = 0; alpha < 26; alpha++)
+            {
+                if(current == shifted[alpha])
+                   index = alpha;
+            }
+        
+        if(index == -1)
+            output += current;
+        else
+            output += alphabet[index];
+    }
+        return output;
     }
 
-    public void shifter(int num){
+    public void shifter(int num)
+    {
+        //Reuse a Caesar cipher if you want to change your encription
+        shifted = new char[26];
 
+        for(int i=0; i<26; i++)
+            shifted[i] = alphabet[(i + num) % 26];
     }
-
+    
 
 }
--- G19.java
+++ G19.java
@@ -17,7 +17,11 @@ public class G19 {
     	System.out.println(cipher.decrypt("ukq zez ep!"));
 
     	//Create a new cipher using the shifter(#) method
+		CaesarCipher newCipher = new CaesarCipher(3);
+
     	//Encrypt and decrypt a message with your new cipher
+		System.out.println("\n" + newCipher.encrypt("happy birthday!"));
+		System.out.println(newCipher.decrypt("khoor"));
 
     }
 
